134a138cf83529c8f254f62e9b7bb4a4fbabf93e
Dean1420 <109102300+Dean1420@users.noreply.github.com>
2026-01-10 23:16:49 +0100

    basic tooltip added

---
+++ Antimonument_Nagasaki/Assets/!_ProjectMain/Scripts/Tooltip/TwoPointsLine.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TwoPointsLine : MonoBehaviour
+{
+    public Transform pointA;
+    public Transform pointB;
+    private LineRenderer line;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        line.positionCount = 2;
+        line.SetPosition(0, pointA.position);
+        line.SetPosition(1, pointB.position);
+    }
+}
