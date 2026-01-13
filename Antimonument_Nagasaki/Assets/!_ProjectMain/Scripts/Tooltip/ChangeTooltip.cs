074453a3136f573ffdbc4e78c3fe03725c0e8ccf
Dean1420 <109102300+Dean1420@users.noreply.github.com>
2026-01-13 18:20:33 +0100

    Tooltips can now be disabled on action

--- Antimonument_Nagasaki/Assets/!_ProjectMain/Scripts/Tooltip/ChangeTooltip.cs
+++ Antimonument_Nagasaki/Assets/!_ProjectMain/Scripts/Tooltip/ChangeTooltip.cs
@@ -4,26 +4,32 @@ using System.Runtime.CompilerServices;
 
 public class ChangeTooltip : MonoBehaviour
 {
-    [SerializeField] private string replacementText;
     [SerializeField] private Transform tooltip;
+    [SerializeField] private string replacementText;
+    [SerializeField] private bool replacementState;
+
     private string originalText;
+    private bool originalState;
     private TextMeshProUGUI tooltipTextMesh;
 
     void Start()
     {
         tooltipTextMesh = tooltip.GetComponentInChildren<TextMeshProUGUI>();
         originalText = tooltipTextMesh.text;
+        originalState = tooltip.gameObject.activeSelf;
     }
 
-    public void replaceText()
+    public void replace()
     {
         tooltipTextMesh.text = replacementText;
+        tooltip.gameObject.SetActive(replacementState);
         Debug.Log("TOOLTIP >>> " + originalText + "changed to " + replacementText);
     }
 
-    public void resetTextToOriginal()
+    public void reset()
     {
         tooltipTextMesh.text = originalText;
+        tooltip.gameObject.SetActive(originalState);
         Debug.Log("TOOLTIP >>> " + replacementText + " changed to " + originalText);
     }
 }
